7b54e700352860e20815c3fbc99a29bed2864d82
Jose Villarroel <josedelvallevs@gmail.com>
2020-09-09 22:24:38 -0500

    Lección 13: Tu primer programa: conversor de monedas

---
+++ conversor.py
@@ -0,0 +1,10 @@
+# El numeral coloca la linea en comentario
+
+pesos = input('Cuantos pesos colombianos tienes?: ')
+pesos = float(pesos)
+valor_dolar = 3875
+dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
+dolares = str(dolares)
+print('Tienes $ ' + dolares + ' dólares ')
+
