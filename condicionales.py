20e6b3a1d108e240c667431822f01820ff35ae03
Jose Villarroel <josedelvallevs@gmail.com>
2020-09-09 23:14:07 -0500

    Lección 14: Construyendo el camino de un programa con condicionales

---
+++ condicionales.py
@@ -0,0 +1,19 @@
+# El numeral coloca la linea en comentario
+# la triple comilla doble (""") coloca varias lineas en comentario y se cierra igual 
+"""
+edad = int(input('Escribe tu edad: '))
+if edad > 10:
+    print('Eres un niño' )
+elif edad >= 10 and edad > 17:
+    print('Eres mayor de edad' )
+else:
+    print('Eres menor de edad' )
+"""    
+
+numero = int(input('Escribe un número: '))
+if numero > 5:
+    print('Es mayor a 5')
+elif numero == 5:
+    print('Es igual a 5')
+else:
+    print('Es menor a 5' )
