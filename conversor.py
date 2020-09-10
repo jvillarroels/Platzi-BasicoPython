613159e4c00fdce8a8c4ecbe82bc3b58615313fe
Jose Villarroel <josedelvallevs@gmail.com>
2020-09-09 23:26:50 -0500

    Lección 15: Varios países en mi conversor de monedas

--- conversor.py
+++ conversor.py
@@ -1,10 +1,28 @@
 # El numeral coloca la linea en comentario
+menu = """BIENVENIDO AL CONVERSOR DE MODEDAS
 
-pesos = input('Cuantos pesos colombianos tienes?: ')
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print('Tienes $ ' + dolares + ' dólares ')
+1 - Pesos Colombianos 
+2 - Pesos Argentinos
+3 - Pesos Mexicanos 
 
+Elige una opción: """
+
+opcion = int(input(menu)) 
+
+if opcion == 1: 
+    pesos = float(input("¿Cuántos pesos Colombianos tienes? "))
+    valor_dolar = 3875
+    dolares = round(pesos /valor_dolar, 2)
+    print("Tienes $" + str(dolares)+ " dólares")
+elif opcion == 2:
+    pesos = float(input("¿Cuántos pesos Argentinos tienes? "))
+    valor_dolar = 65
+    dolares = round(pesos /valor_dolar, 2)
+    print("Tienes $" + str(dolares)+ " dólares")
+elif opcion == 3:
+    pesos = float(input("¿Cuántos pesos Mexicanos tienes? "))
+    valor_dolar = 24
+    dolares = round(pesos /valor_dolar, 2)
+    print("Tienes $" + str(dolares)+ " dólares")
+else:
+    print("Ingresa una opción correcta")
